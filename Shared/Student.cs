4a29f6113dc640d9a8b3a90860fad2293e85b155
death <deth@itu.dk>
2021-11-26 13:26:20 +0100

    Minor name changes

--- Shared/Student.cs
+++ Shared/Student.cs
@@ -4,11 +4,14 @@ using System.ComponentModel.DataAnnotations;
 public class Student : CurrentUser
 {
     [Key]
-    public int Id {get; set;}
+    public int ID {get; set;}
+
     [Required(ErrorMessage = "Name is required field")]
     public string name { get; set; }
 
     [Required(ErrorMessage = "Email required")]
+    [EmailAddress]
+    [MaxLength(100)]
     public string Email { get; set; }
 
 
