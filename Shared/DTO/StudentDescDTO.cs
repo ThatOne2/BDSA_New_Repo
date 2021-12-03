1566da0eeda95ce4e703bb259b5d79fde40eb828
daso <daso@itu.dk>
2021-12-03 13:26:03 +0100

    DTOs made and removed old autogenerated pages

---
+++ Shared/DTO/StudentDescDTO.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrialProject.Shared.DTO
+{
+	public class StudentDescDTO
+	{
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Student name is required field")]
+        [MaxLength(50)]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+    }
+}
---
+++ Shared/DTO/StudentPreviewDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrialProject.Shared.DTO
+{
+	public class StudentPreviewDTO
+	{
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Student name is required field")]
+        [MaxLength(50)]
+        public string name { get; set; }
+    }
+}
