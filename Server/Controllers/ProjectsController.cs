091286786cb6cc4577ef394376c74fb49d667beb
Robin Ask Rohr Halberg <robin.hal6erg@gmail.com>
2021-12-11 17:54:58 +0100

    Added TODOs and dummy await to remove warnings

--- Server/Controllers/ProjectsController.cs
+++ Server/Controllers/ProjectsController.cs
@@ -26,15 +26,19 @@ public class ProjectController : ControllerBase {
         //if (s == null) { return HttpStatusCode.BadRequest;}
 
         var tags = new List<Tag>();
-        foreach (TagsEnums tag in p.Tags) {
-            tags.Add(new Tag { Name = tag.ToString() });
+        if (p.Tags != null)
+        {
+            foreach (TagsEnums tag in p.Tags)
+            {
+                tags.Add(new Tag { Name = tag.ToString() });
+            }
         }
 
         Project project = new Project {
             name = p.name,
             longDescription = p.longDescription,
             shortDescription = p.shortDescription,
-            SupervisorID = 1, // TODO: Change
+            SupervisorID = 1, // TODO: Input proper supervisor id
             Tags = tags,
             ProjectStatus = Status.Ongoing
         };
@@ -53,7 +57,9 @@ public class ProjectController : ControllerBase {
     [HttpGet("{id}")]
     public async Task<IActionResult> ReadDescProjectById(int id) {
 
-        
+        // TODO: Find where to put await
+        await Task.FromResult(0);
+
         var p =  _context.Projects!.Include(tag => tag.Tags).Join(_context.Supervisors,
                                                                             p => p.SupervisorID,
                                                                             ss => ss.ID,
@@ -71,7 +77,7 @@ public class ProjectController : ControllerBase {
         var tagList = new List<string>();
         if (p == null)
         {
-            return BadRequest();
+            return BadRequest(); 
         }
         else
         {
@@ -97,107 +103,106 @@ public class ProjectController : ControllerBase {
     [HttpGet]
     public IEnumerable<ProjectPreviewDTO> GetAllProjects() {
         var list = new List< ProjectPreviewDTO>();
-             foreach (var p in _context.Projects!.Include(tag => tag.Tags).Join(_context.Supervisors!,
-                                                                                p => p.SupervisorID,
-                                                                                ss => ss.ID,
-                                                                                (p,ss) => new {
-                                                                                    Supervisor = ss.name,
-                                                                                    shortDesc = p.shortDescription,
-                                                                                    ID = p.ID,
-                                                                                    Tags = p.Tags,
-                                                                                    Name = p.name
-                                                                                }))
-            {
-               
-                var tagList = new List<string>();
-                foreach (var t in p.Tags!) {
-                    tagList.Add(t.Name!);
-                }
+        foreach (var p in _context.Projects!.Include(tag => tag.Tags).Join(_context.Supervisors!,
+                                                                            p => p.SupervisorID,
+                                                                            ss => ss.ID,
+                                                                            (p,ss) => new {
+                                                                                Supervisor = ss.name,
+                                                                                shortDesc = p.shortDescription,
+                                                                                ID = p.ID,
+                                                                                Tags = p.Tags,
+                                                                                Name = p.name
+                                                                            }))
+        {
+            var tagList = new List<string>();
+            foreach (var t in p.Tags!) {
+                tagList.Add(t.Name!);
+            }
 
-                var ProjDTO = new  ProjectPreviewDTO{SupervisorName = p.Supervisor, name = p.Name, shortDescription = p.shortDesc, ID = p.ID, Tags = tagList};
-                list.Add(ProjDTO);
-            }
-            if (list.Any())
-            {
-                return list.ToArray();
-            }
-            else
-            {
-                return null!;
-            } 
+            var ProjDTO = new  ProjectPreviewDTO{SupervisorName = p.Supervisor, name = p.Name, shortDescription = p.shortDesc, ID = p.ID, Tags = tagList};
+            list.Add(ProjDTO);
+        }
+
+        if (list.Any())
+        {
+            return list.ToArray();
+        }
+        else
+        {
+            return null!;
+        } 
     }
     
 
    //Returns a list of all projects a Supervisor has posted(Maybe using  yield return?)
    [HttpGet("supervisor/{supervisorID}")]
     public  IEnumerable<ProjectPreviewDTO> ReadAllProjectsPostedBySupervisor(int supervisorID){
-          var list = new List<ProjectPreviewDTO>();
-             foreach (var p in _context.Projects!.Include(tag => tag.Tags).Join(_context.Supervisors!,
-                                                                                p => p.SupervisorID,
-                                                                                ss => ss.ID,
-                                                                                (p,ss) => new {
-                                                                                    Supervisor = ss.name,
-                                                                                    supervisorID = ss.ID,
-                                                                                    shortDesc = p.shortDescription,
-                                                                                    ID = p.ID,
-                                                                                    Tags = p.Tags,
-                                                                                    Name = p.name
-                                                                                }).Where(x => x.supervisorID == supervisorID))
-            {
-
-                var tagList = new List<string>();
-                foreach (var t in p.Tags!) {
-                    tagList.Add(t.Name!);
-                }
-
-                var ProjDTO = new  ProjectPreviewDTO{SupervisorName = p.Supervisor, name = p.Name, shortDescription = p.shortDesc, ID = p.ID, Tags = tagList};
-                list.Add(ProjDTO);
+        var list = new List<ProjectPreviewDTO>();
+        foreach (var p in _context.Projects!.Include(tag => tag.Tags).Join(_context.Supervisors!,
+                                                                        p => p.SupervisorID,
+                                                                        ss => ss.ID,
+                                                                        (p,ss) => new {
+                                                                            Supervisor = ss.name,
+                                                                            supervisorID = ss.ID,
+                                                                            shortDesc = p.shortDescription,
+                                                                            ID = p.ID,
+                                                                            Tags = p.Tags,
+                                                                            Name = p.name
+                                                                        }).Where(x => x.supervisorID == supervisorID))
+        {
+            var tagList = new List<string>();
+            foreach (var t in p.Tags!) {
+                tagList.Add(t.Name!);
             }
-            if (list.Any())
-            {
-                return list.ToArray();
-            }
-            else
-            {
-                return null!;
-            } 
+
+            var ProjDTO = new  ProjectPreviewDTO{SupervisorName = p.Supervisor, name = p.Name, shortDescription = p.shortDesc, ID = p.ID, Tags = tagList};
+            list.Add(ProjDTO);
+        }
+
+        if (list.Any())
+        {
+            return list.ToArray();
+        }
+        else
+        {
+            return null!;
+        } 
     } 
 
     //Returns a list of projects that has the selected tag(s)  (Maybe using  yield return?)
     [HttpGet("tag/{tag}")]
     public IEnumerable<ProjectPreviewDTO>? ReadProjectListByTag(string tag){
-             var list = new List<ProjectPreviewDTO>();
-             foreach (var p in _context.Projects!.Include(xtag => xtag.Tags).Join(_context.Supervisors!,
-                                                                                p => p.SupervisorID,
-                                                                                ss => ss.ID,
-                                                                                (p,ss) => new {
-                                                                                    Supervisor = ss.name,
-                                                                                    supervisorID = ss.ID,
-                                                                                    shortDesc = p.shortDescription,
-                                                                                    ID = p.ID,
-                                                                                    Tags = p.Tags,
-                                                                                    Name = p.name
-                                                                                }).Where(x => x.Tags.Any(ptag => ptag.Name == tag)))
-//.Where(x => x.Tags!.Any(tag => tag.Name!.ToString() == t))
-
-            {
-                var tagList = new List<string>();
-                foreach (var ytag in p.Tags!) {
-                    tagList.Add(ytag.Name!);
-                }
+        var list = new List<ProjectPreviewDTO>();
+        foreach (var p in _context.Projects!.Include(xtag => xtag.Tags).Join(_context.Supervisors!,
+                                                                        p => p.SupervisorID,
+                                                                        ss => ss.ID,
+                                                                        (p,ss) => new {
+                                                                            Supervisor = ss.name,
+                                                                            supervisorID = ss.ID,
+                                                                            shortDesc = p.shortDescription,
+                                                                            ID = p.ID,
+                                                                            Tags = p.Tags,
+                                                                            Name = p.name
+                                                                        }).Where(x => x.Tags.Any(ptag => ptag.Name == tag)))
+            //.Where(x => x.Tags!.Any(tag => tag.Name!.ToString() == t))
+        {
+            var tagList = new List<string>();
+            foreach (var ytag in p.Tags!) {
+                tagList.Add(ytag.Name!);
+            }
                
-                var ProjDTO = new  ProjectPreviewDTO{SupervisorName = p.Supervisor, name = p.Name, shortDescription = p.shortDesc, ID = p.ID, Tags = tagList};
-                list.Add(ProjDTO);
-            }
-            if (list.Any())
-            {
-                return list.ToArray();
-            }
-            else
-            {
-              
-                return null!;
-            } 
+            var ProjDTO = new  ProjectPreviewDTO{SupervisorName = p.Supervisor, name = p.Name, shortDescription = p.shortDesc, ID = p.ID, Tags = tagList};
+            list.Add(ProjDTO);
+        }
+
+        if (list.Any())
+        {
+            return list.ToArray();
+        }
+        else
+        {
+            return null!;
+        } 
     }
 
 
