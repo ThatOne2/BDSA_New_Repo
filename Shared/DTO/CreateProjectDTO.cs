981eecaa2ac45c970db6e7fd203fae7c3bb9536b
death <deth@itu.dk>
2021-12-11 10:49:45 +0100

    Tags visual in allprojects and major cleanup

--- Shared/DTO/CreateProjectDTO.cs
+++ Shared/DTO/CreateProjectDTO.cs
@@ -7,20 +7,20 @@ namespace TrialProject.Shared.DTO
 
         [Required(ErrorMessage = "Project name is required field")]
         [MaxLength(50)]
-        public string name { get; set; }
+        public string? name { get; set; }
 
         [Required(ErrorMessage = "Teacher name is required field")]
         [MaxLength(50)]
         public int SupervisorID { get; set; }
 
         [Required(ErrorMessage = "Description is required")]
-        public string shortDescription { get; set; }
+        public string? shortDescription { get; set; }
 
         [Required(ErrorMessage = "Description is required")]
-        public string longDescription { get; set; }
+        public string? longDescription { get; set; }
 
         [Required(ErrorMessage = "A least one tag required")]
-        public ICollection<Tag> Tags { get; set; }
+        public ICollection<Tag>? Tags { get; set; }
 
     }
 }
