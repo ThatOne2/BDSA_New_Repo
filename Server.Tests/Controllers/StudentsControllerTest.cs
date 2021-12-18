6c951597c62cab7444dfcb7a725801730ba71bf4
Robin Ask Rohr Halberg <robin.hal6erg@gmail.com>
2021-12-18 11:55:39 +0100

    StudentControllerTests and SupervisorControllerTests are done

--- Server.Tests/Controllers/StudentsControllerTest.cs
+++ Server.Tests/Controllers/StudentsControllerTest.cs
@@ -21,71 +21,160 @@ public class StudentControllerTests
  
         
 
-        public StudentControllerTests()
-        {
-            var connection = new SqliteConnection("Data Source=:memory:");
-            connection.Open();
-            var builder = new DbContextOptionsBuilder<TrialProject.Server.Controllers.DataContext>();
-            builder.UseSqlite(connection);
-            var context = new TrialProject.Server.Controllers.DataContext(builder.Options);
-            context.Database.EnsureCreated();
-
-            var logger = new Mock<ILogger<StudentsController>>();
-
-            repo = new StudentsController(logger.Object, context);
-
-            var Stundent = new Student{ID = 1, name = "Bru", Email = "Mart@Brumail.com"};
-
-            context.Students!.Add(Stundent);
-
-            context.SaveChanges();
-
-        }
-
-        [Fact]
-    public async Task Read_Student_by_Id_Given_Negative_Returns_Bad_Request()
+    public StudentControllerTests()
     {
-        //Arrange
+        var connection = new SqliteConnection("Data Source=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<TrialProject.Server.Controllers.DataContext>();
+        builder.UseSqlite(connection);
+        var context = new TrialProject.Server.Controllers.DataContext(builder.Options);
+        context.Database.EnsureCreated();
+
         var logger = new Mock<ILogger<StudentsController>>();
 
-        //Act
-        var result = repo.ReadStudentDescById(-1).Result.Result;
+        repo = new StudentsController(logger.Object, context);
 
-        //Assert
-        Assert.IsType<BadRequestResult>(result);
-    } 
+        var Stundent = new Student{ID = 1, name = "Bru", Email = "Mart@Brumail.com"};
 
-    [Fact]
-    public async Task Student_Reading_By_ID(){
+        context.Students!.Add(Stundent);
 
-        //Arrange
-        var logger = new Mock<ILogger<StudentsController>>();
-        var expected = new StudentDescDTO{ID = 1, name = "Bru", Email = "Mart@Brumail.com"};
-
-        //Act
-        var project = repo.ReadStudentDescById(1).Result.Result as OkObjectResult;
-        var actual = project.Value;
-
-        //Assert
-        Assert.Equal(expected.ToString(), actual.ToString());
+        context.SaveChanges();
 
     }
 
-
-
+    // ==============================================================================================
+    // CreateStudent
+    // ==============================================================================================
 
     [Fact]
-    public async Task Test()
+    public void Create_Student_Already_Exists_Returns_250()
     {
         //Arrange
-    
-        
+        var s1 = new CreateStudentDTO()
+        {
+            name = "Sasha",
+            Email = "Sasha@email.com"
+        };
+        var s2 = new CreateStudentDTO()
+        {
+            name = "Emily",
+            Email = "Sasha@email.com"
+        };
+
         //Act
-   
+        var a = repo.CreateStudent(s1);
+        var result = repo.CreateStudent(s2).Result.Result as ObjectResult;
 
         //Assert
-        //Assert.Equal(1,2);
-    } 
+        Assert.Equal(250, result!.StatusCode);
+    }
 
-    
+    [Fact]
+    public void Create_Student_Proper_Returns_Created()
+    {
+        //Arrange
+        var s = new CreateStudentDTO()
+        {
+            name = "Sasha",
+            Email = "Sasha@email.com"
+        };
+
+        //Act
+        var result = repo.CreateStudent(s).Result.Result;
+
+        //Assert
+        Assert.IsType<CreatedResult>(result);
+    }
+
+    [Fact]
+    public void Create_Student_No_Name_Returns_500()
+    {
+        //Arrange
+        var s = new CreateStudentDTO() { Email = "Sasha@email.com" };
+
+        //Act
+        var result = repo.CreateStudent(s).Result.Result as StatusCodeResult;
+
+        //Assert
+        Assert.Equal(500, result!.StatusCode);
+    }
+
+    [Fact]
+    public void Create_Student_No_Email_Returns_500()
+    {
+        //Arrange
+        var s = new CreateStudentDTO() { name = "Sasha" };
+
+        //Act
+        var result = repo.CreateStudent(s).Result.Result as StatusCodeResult;
+
+        //Assert
+        Assert.Equal(500, result!.StatusCode);
+    }
+
+    // ==============================================================================================
+    // ReadStudentDescByID
+    // ==============================================================================================
+
+    [Fact]
+    public void Read_Student_By_ID_Doesnt_Exist_Returns_BadRequest()
+    {
+        //Arrange
+
+
+        //Act
+        var result = repo.ReadStudentDescById(10)!.Result.Result;
+
+        //Assert
+        Assert.IsType<BadRequestResult>(result);
+    }
+
+    [Fact]
+    public void Read_Student_By_ID_Exists_Returns_StudentDescDTO()
+    {
+        //Arrange
+        var expected = new StudentDescDTO
+        {
+            ID = 1,
+            name = "Bru",
+            Email = "Mart@Brumail.com"
+        };
+
+        //Act
+        var result = repo.ReadStudentDescById(1)!.Result.Result;
+
+        //Assert
+        var r = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(expected.ToString(), r.Value!.ToString());
+    }
+
+    // ==============================================================================================
+    // DeleteStudent
+    // ==============================================================================================
+
+    [Fact]
+    public void Delete_Student_Doesnt_Exist_Returns_InternalServerError()
+    {
+        //Arrange
+
+        //Act
+        var result = repo.DeleteStudent(10);
+
+        //Assert
+        Assert.Equal(System.Net.HttpStatusCode.InternalServerError, result);
+    }
+
+    [Fact]
+    public void Delete_Student_Exists_Returns_OK()
+    {
+        //Arrange
+
+        //Act
+        var result = repo.DeleteStudent(1);
+
+        //Assert
+        Assert.Equal(System.Net.HttpStatusCode.OK, result);
+    }
+
+
 }
--- Server.Tests/Controllers/SupervisorControllerTest.cs
+++ Server.Tests/Controllers/SupervisorControllerTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TrialProject.Server.Controllers;
 using TrialProject.Shared.DTO;
+using TrialProject.Shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Server.Tests.Controllers;
@@ -19,24 +20,35 @@ public class SupervisorControllerTest
  
         
 
-        public SupervisorControllerTest()
+    public SupervisorControllerTest()
+    {
+        var connection = new SqliteConnection("Data Source=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<TrialProject.Server.Controllers.DataContext>();
+        builder.UseSqlite(connection);
+        var context = new TrialProject.Server.Controllers.DataContext(builder.Options);
+        context.Database.EnsureCreated();
+
+        var logger = new Mock<ILogger<SupervisorsController>>();
+
+        var s = new Supervisor
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
-            connection.Open();
-            var builder = new DbContextOptionsBuilder<TrialProject.Server.Controllers.DataContext>();
-            builder.UseSqlite(connection);
-            var context = new TrialProject.Server.Controllers.DataContext(builder.Options);
-            context.Database.EnsureCreated();
+            name = "Martin",
+            Email = "Martin@email.com"
+        };
+        context.Supervisors!.Add(s);
 
-            var logger = new Mock<ILogger<SupervisorsController>>();
+        repo = new SupervisorsController(logger.Object, context);
 
-            repo = new SupervisorsController(logger.Object, context);
+        context.SaveChanges();
+    }
 
-        }
-
+    // ==============================================================================================
+    // CreateSupervisor
+    // ==============================================================================================
 
     [Fact]
-    public async Task Create_Supervisor_Already_Exists_Returns_250()
+    public void Create_Supervisor_Already_Exists_Returns_250()
     {
         //Arrange
         var s1 = new CreateSupervisorDTO()
@@ -59,7 +71,7 @@ public class SupervisorControllerTest
     }
 
     [Fact]
-    public async Task Create_Supervisor_Proper_Returns_Created()
+    public void Create_Supervisor_Proper_Returns_Created()
     {
         //Arrange
         var s = new CreateSupervisorDTO()
@@ -76,7 +88,7 @@ public class SupervisorControllerTest
     }
 
     [Fact]
-    public async Task Create_Supervisor_No_Name_Returns_500()
+    public void Create_Supervisor_No_Name_Returns_500()
     {
         //Arrange
         var s = new CreateSupervisorDTO() { Email = "Sasha@email.com" };
@@ -90,7 +102,7 @@ public class SupervisorControllerTest
     }
 
     [Fact]
-    public async Task Create_Supervisor_No_Email_Returns_500()
+    public void Create_Supervisor_No_Email_Returns_500()
     {
         //Arrange
         var s = new CreateSupervisorDTO() { name = "Sasha" };
@@ -100,5 +112,69 @@ public class SupervisorControllerTest
 
         //Assert
         Assert.Equal(500, result!.StatusCode);
+    }
+
+    // ==============================================================================================
+    // ReadSupervisorDescByID
+    // ==============================================================================================
+
+    [Fact]
+    public void Read_Supervisor_By_ID_Doesnt_Exist_Returns_BadRequest()
+    {
+        //Arrange
+
+
+        //Act
+        var result = repo.ReadSupervisorDescById(10)!.Result.Result;
+
+        //Assert
+        Assert.IsType<BadRequestResult>(result);
+    }
+
+    [Fact]
+    public void Read_Supervisor_By_ID_Exists_Returns_SupervisorDescDTO()
+    {
+        //Arrange
+        var expected = new SupervisorDescDTO
+        {
+            ID = 1,
+            name = "Martin",
+            Email = "Martin@email.com"
+        };
+
+        //Act
+        var result = repo.ReadSupervisorDescById(1)!.Result.Result;
+
+        //Assert
+        var r = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(expected.ToString(), r.Value!.ToString());
+    }
+
+    // ==============================================================================================
+    // DeleteSupervisor
+    // ==============================================================================================
+
+    [Fact]
+    public void Delete_Supervisor_Doesnt_Exist_Returns_InternalServerError()
+    {
+        //Arrange
+
+        //Act
+        var result = repo.DeleteSupervisor(10);
+
+        //Assert
+        Assert.Equal(System.Net.HttpStatusCode.InternalServerError, result);
+    }
+
+    [Fact]
+    public void Delete_Supervisor_Exists_Returns_OK()
+    {
+        //Arrange
+
+        //Act
+        var result = repo.DeleteSupervisor(1);
+
+        //Assert
+        Assert.Equal(System.Net.HttpStatusCode.OK, result);
     }
 }
